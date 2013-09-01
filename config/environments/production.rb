c4e9ee8336a2e86d04807fd36927424e2ad66733
shock_one <shockone89@gmail.com>
2013-09-02 02:34:50 +0300

    Trying to fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,14 @@ Oasit::Application.configure do
   config.action_mailer.default_url_options = { :host => 'oasit.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'oasitnoreply',
+      :password             => 'oasitnoreplyoasitnoreply',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
 
 end
